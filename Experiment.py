2b91a186da4f52e8668e161b837699823839af87
zhangbingliang2019 <zhangbl19@mails.tsinghua.edu.cn>
2020-11-28 01:03:54 +0800

    simple evaluation

---
+++ Experiment.py
@@ -0,0 +1,35 @@
+import Agent
+import Board
+import Game
+import matplotlib.pyplot as plt
+from DataStorage import dataProcessor
+
+
+class Experiment():
+    def __init__(self, args=None):
+        self.args = args
+
+    def getWinScore(self, game, agent):
+        win = 0
+        for i in range(self.args.numOfEvaluations):
+            if game.run() == agent:
+                win += 1
+        return win / self.args.numOfEvaluations
+
+    def selfplayWithDifferentNumOfIterations(self):
+        model = dataProcessor.loadNetwork(self.args)
+        winRate = []
+        for iter in range(5, 50, 5):
+            agent = Agent.IntelligentAgent(iter, model)
+            board = Board.Board(self.args.size, self.args.numberForWin)
+            game = Game.Game(agent, agent, board)
+            winRate.append(self.getWinScore(game, agent))
+            print("Iterations: %d, WinRate: %.2f" % (iter, winRate[-1]))
+
+        plt.figure()
+        plt.xlabel("Number of Iterations per Movem")
+        plt.ylabel("Win Rate for First Player")
+        plt.title("The must-win-strategy in 6*6 board with 3 in a row is learnable")
+        plt.plot(range(5, 50, 5), winRate, color='blue', linestyle='-')
+        plt.savefig('figure/6-6 3 must-win-strategy.png')
+        plt.show()
